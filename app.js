704c8e87c05d77233d5baa95bce3bbe56de44a9e
Robert <rspatz@live.com>
2024-03-10 13:39:32 -0500

    Added high and low range prompts to guessing game

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ while (!maxNumber) {
 
 const targetNum = Math.floor(Math.random() * maxNumber) + 1;
 console.log(targetNum);
+
+let guess = parseInt(prompt('Enter your first guess!'));
+while (guess !== targetNum) {
+  if (guess > targetNum) {
+    guess = parseInt(prompt('Too high! Enter a new guess!'));
+  } else {
+    guess = parseInt(prompt('Too low! Enter a new guess!'));
+  }
+}
+console.log('You GOT it!');
